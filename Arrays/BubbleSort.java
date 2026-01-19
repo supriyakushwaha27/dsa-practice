3a08381236eb46dfd061b86dc21e5e7833c3abe6
Supriya Kushwaha <supriyakushwaha449@gmail.com>
2026-01-19 23:44:25 +0530

    bubble sort

---
+++ Arrays/BubbleSort.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int arr[]) {
+
+        for (int i = 0; i <= arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = { 1, 3, 4, 2 };
+        System.out.println("before sort:");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+
+        bubbleSort(arr);
+
+        System.out.println();
+        System.out.println("after sort:");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
